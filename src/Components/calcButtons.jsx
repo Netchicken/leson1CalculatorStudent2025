c2176bf6a77230ee66b308e4a0fc2e17c381e679
Gary Dix <intelproof@gmail.com>
2025-06-11 11:47:39 +1200

    basic version working!

--- src/Components/calcButtons.jsx
+++ src/Components/calcButtons.jsx
@@ -2,34 +2,48 @@ import React from "react";
 import { useState } from "react";
 import Inputs from "./Inputs"; // Assuming you have an Inputs component for input fields
 import SingleButton from "./SingleButton"; // Assuming you have a SingleButton component for buttons
+import ButtonKeypad from "./ButtonKeypad"; // Assuming you have a ButtonKeypad component for number buttons
+import { evaluate } from "mathjs"; // Importing mathjs for calculations
 
 export const CalcButtons = () => {
   const [answer, setAnswer] = useState("");
   const [first, setFirst] = useState(10);
   const [second, setSecond] = useState(22);
+  const [trigger, setTrigger] = useState(false);
+  const [symbolUsed, setSymbolUsed] = useState(""); // To store the operator
+
+  let symbols = ["+", "-", "X", "/"]; // Array of symbols for operations
+
+  //we are passing in the value of the button pressed
+  const keyInput = (value) => {
+    console.log(value + " " + trigger);
+
+    if (trigger === false) {
+      setFirst(Number(first + String(value))); //concatenate the numbers then save as number
+      console.log("first value", value);
+    }
+
+    if (trigger === true) {
+      setSecond(Number(second + String(value))); //concatenate the numbers then save as number
+    }
+  };
 
   //Value = +-/X
   const Calculate = (value) => {
     console.log(value);
+    setTrigger(true);
+    console.log("trigger run", value + " " + trigger);
 
-    switch (value) {
-      case "+":
-        setAnswer(Number(first + second)); //Number is need to stop concatination
-        break;
-      case "-":
-        setAnswer(first - second);
-        break;
-      case "X":
-        setAnswer(first * second);
-        break;
-      case "/":
-        setAnswer(first / second);
-        break;
-      default:
-        setAnswer("Invalid operation");
+    if (symbols.includes(value)) {
+      //yay its  a symbol
+      setSymbolUsed(value); //save it to use below
+    }
+    if (value === "=") {
+      setAnswer(evaluate(`${first} ${symbolUsed} ${second}`));
+
+      // 5 + 4
     }
   };
-
   return (
     <div>
       <Inputs
@@ -42,7 +56,7 @@ export const CalcButtons = () => {
 
       <div className="calc-box">
         <p>
-          {first} + {second} = {answer}
+          {first} {symbolUsed} {second} = {answer}
         </p>
 
         <div className="digits">
@@ -50,7 +64,9 @@ export const CalcButtons = () => {
           <SingleButton symbol="-" Calculate={Calculate}></SingleButton>
           <SingleButton symbol="/" Calculate={Calculate}></SingleButton>
           <SingleButton symbol="X" Calculate={Calculate}></SingleButton>
+          <SingleButton symbol="=" Calculate={Calculate}></SingleButton>
         </div>
+        <ButtonKeypad keyInput={keyInput} />
       </div>
     </div>
   );
